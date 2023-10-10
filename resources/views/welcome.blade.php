268e92dd83d7fab3a5e72aa976cca5e085fd947f
nguyenduchoan <nguyenduchoan.wru@gmail.com>
2023-10-10 10:24:40 +0700

    add file Category model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -418,39 +418,7 @@
     </head>
 
     <body>
-        @extends('layouts.master')
-        @section('main')
-            <div class="jumbotron">
-                <div class="container">
-                    <h1>Danh mục</h1>
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- {{ dd($categories) }} -->
-                            @foreach ($categories as $cat)
-                                {{-- {{ dd('check') }} --}}
-                                <tr>
-                                    <td>{{ $cat->id }}</td>
-                                    <td>{{ $cat->name }}</td>
-                                    <td>{{ $cat->status == 0 ? 'Tạm ẩn' : 'Hiển thị' }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    <hr>
-                    <hr>
-                    <!-- <div>
-                        {{ $categories->links() }}
-                    </div> -->
-                </div>
-            </div>
-        @stop()
+            Welcome
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 
